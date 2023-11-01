b55efaf8be2fbb6c416559380d1896998d05f97b
Ost <vojvol@post.cz>
2023-11-01 15:55:12 +0100

    2d array works now

--- 1_20102023/2d_pole.py
+++ 1_20102023/2d_pole.py
@@ -1,10 +1,14 @@
+#Input data
 array_2D = [[1, 2, 3], [4, 5, 6], [7, 20, 9]]
+number_distance = 3
 
+#Starting values
 number_count = 0
 addition = 0
 all_numbers = []
 final_numbers = []
 
+#Getting an average
 for row in array_2D:
     for colum in row:
         number_count+=1
@@ -13,22 +17,28 @@ for row in array_2D:
 
 average = addition/number_count
 
+#Finding numbers in correct distance
 for number in all_numbers:
-    if abs(number-average) <= 3:
+    if abs(number-average) <= number_distance:
         final_numbers.append(number)
-        
-print(final_numbers)
-print(average)
+
+#Data printing
+print("Vstupní údaje")
+for row in array_2D:
+    print(row)
+print(f"Průměrná hodnota: {average}")        
+print(f"Čísla ve vzdálenosti {number_distance} od průměrné hodnoty: {final_numbers}")
+
+# Getting the cords
 cords = ""
-print(len(final_numbers))
-while 0 < len(final_numbers):
-    for row_cords in enumerate(array_2D):
-        #print(row_cords)
-        # FIXME stuckne se v nekonečneém loopu po proběhnutí jednoho loopu
+while True:
+    if(len(final_numbers) == 0):
+        break
+    # first is a number, second is na array
+    for row_cords, row in enumerate(array_2D):
         for colum_cords, colum in enumerate(row):
             if colum in final_numbers:
                 final_numbers.remove(colum)
-                print(colum)
-                print(final_numbers)
                 cords += f"Číslo: {colum}, Souřadnice řádku {row_cords}, Souřadnice sloupce: {colum_cords}\n"
+#Final output
 print(cords)
